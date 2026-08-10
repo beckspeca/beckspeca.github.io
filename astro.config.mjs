45a22394607d6f9377f2b664624a0356b09fce66
shinjaewon <shinjaewon@shinjaewons-MacBook-Air.local>
2026-08-11 01:27:56 +0900

    Update config for root domain deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import { defineConfig, fontProviders } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://beckspeca.github.io',
-	base: 'Portfolio',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
