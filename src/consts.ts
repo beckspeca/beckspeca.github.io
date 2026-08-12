026f824062683a4d0fa3cd8c86159bef4fd7bfef
shinjaewon <shinjaewon@shinjaewons-MacBook-Air.local>
2026-08-12 20:23:06 +0900

    Redesign site as data analyst portfolio

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'JAEWON.LOG';
-export const SITE_DESCRIPTION = '배우고 만들며 발견한 것을 기록하는 개발 블로그';
+export const SITE_TITLE = 'JAEWON.DATA';
+export const SITE_DESCRIPTION = '데이터에서 질문을 발견하고 근거 있는 답을 만드는 데이터 분석가 신재원의 포트폴리오';
 export const AUTHOR = '신재원';
 export const GITHUB_URL = 'https://github.com/beckspeca';
